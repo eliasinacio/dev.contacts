a07a9f931e4ac639afb14b6876bc7117ebf04adf
eliasinacio <el1as.1nac1o.ch@gmail.com>
2021-05-04 16:15:24 -0300

    adicionando arquivo js e inicio de tratamento de dados

---
+++ scripts.js
@@ -0,0 +1,10 @@
+contact = {
+  "id": 1,
+  "name": "Yuji Itadori",
+  "phone": "88988888888",
+  "email": "yujiitadori@mail.com",
+  "github": "https://github.com/eliasinacio",
+  "instagram": "https://instagram.com/eu_liaaas",
+  "twitter": "https://twitter.com/eliasinacio_1",
+  "linkedin": "https://linkedin.com/in/eliasinacio0/"
+}

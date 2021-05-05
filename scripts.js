9aeae78180ed3193c4d10a8bb7b559bfc8cb12df
eliasinacio <el1as.1nac1o.ch@gmail.com>
2021-05-05 16:18:48 -0300

    modal criado, botoes de abrir e fechar modal funcionando.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
-contact = {
-  "id": 1,
-  "name": "Yuji Itadori",
-  "phone": "88988888888",
-  "email": "yujiitadori@mail.com",
-  "github": "https://github.com/eliasinacio",
-  "instagram": "https://instagram.com/eu_liaaas",
-  "twitter": "https://twitter.com/eliasinacio_1",
-  "linkedin": "https://linkedin.com/in/eliasinacio0/"
-}
+// MODAL ATIVATION 
+const addContactBtn = document.querySelector('.add-contact h1');
+const modalBg = document.querySelector('.modal-background');
+const cancelBtn = document.getElementById('cancelBtn')
+
+addContactBtn.addEventListener('click', () => {
+  modalBg.classList.add('activated');
+})
+
+cancelBtn.addEventListener('click', () => {
+  modalBg.classList.remove('activated');
+})

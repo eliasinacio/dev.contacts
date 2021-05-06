60b69e9d2d046e818c4ef4a1730c013a77c93660
eliasinacio <el1as.1nac1o.ch@gmail.com>
2021-05-05 21:56:24 -0300

    localStorage funcionando, tentativa de incorporar falhou mas está bem encaminhado.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,40 @@
 // MODAL ATIVATION 
-const addContactBtn = document.querySelector('.add-contact h1');
-const modalBg = document.querySelector('.modal-background');
-const cancelBtn = document.getElementById('cancelBtn')
+const Modal = {
+  open() {
+    document.querySelector('.modal-background')
+    .classList.add('activated');
+  },
 
-addContactBtn.addEventListener('click', () => {
-  modalBg.classList.add('activated');
-})
+  close() {
+    document.querySelector('.modal-background')
+    .classList.remove('activated');
+  }
+}
 
-cancelBtn.addEventListener('click', () => {
-  modalBg.classList.remove('activated');
-})
+const transactions = [{
+  "name": "Yuji Itadori",
+  "phone": "88988888888",
+  "email": "yujiitadori@mail.com",
+  "github": "https://github.com/eliasinacio",
+  "instagram": "https://instagram.com/eu_liaaas",
+  "twitter": "https://twitter.com/eliasinacio_1",
+  "linkedin": "https://linkedin.com/in/eliasinacio0/"
+},
+{
+  "name": "Nobara",
+  "phone": "77823456378",
+  "email": "nobarakugisaki@mail.com",
+  "github": "https://github.com/eliasinacio",
+  "instagram": "https://instagram.com/eu_liaaas",
+  "twitter": "https://twitter.com/eliasinacio_1",
+  "linkedin": "https://linkedin.com/in/eliasinacio0/"
+}]
+
+// tentativa de receber os dados do Local Storage e adicionar ao html :/
+// LOCALSTORAGE
+
+localStorage.setItem("contacts", JSON.stringify(transactions));
+
+const newContactCard = document.createElement('div');
+newContactCard.classList.add('contact-card');
+document.querySelector(".contacts").appendChild(newContactCard);

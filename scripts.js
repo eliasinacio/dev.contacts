80aa8321cb7082c2c02bcec98c1b48c2a9ad0cb0
eliasinacio <el1as.1nac1o.ch@gmail.com>
2021-05-07 16:58:30 -0300

    modais mobile finalizados.

--- scripts.js
+++ scripts.js
@@ -25,26 +25,6 @@ const contacts = {
   all: Storage.get()
 }
 
-
-// const contacts = [
-//   {
-//     "icon": "https://i.pinimg.com/originals/b2/5a/ec/b25aecc9ecc908ec35e3f0385eb9d60a.jpg",
-//     "name": "Yuji Itadori",
-//     "phone": "099393837298",
-//     "email": "yujiitadorigojo@mail.com"
-//   },{
-//     "icon": "https://i.pinimg.com/originals/ee/49/54/ee4954e867697ab3ced9ff6f5842a7f9.jpg",
-//     "name": "Nobara Kugisaki",
-//     "phone": "77823456378",
-//     "email": "nobarakugisaki@mail.com"
-//   },{
-//     "icon": "https://i.pinimg.com/originals/12/ae/ba/12aeba8e7736cacd19fa7299a8ebb8a9.jpg",
-//     "name": "Fushiguro Megumi",
-//     "phone": "556637829",
-//     "email": "mmegumifushiguro00@mail.com"
-//   }]
-// localStorage.setItem("contacts", JSON.stringify(contacts));
-
 contacts.all.forEach( (contact) => {
   const card = document.createElement('div')
   card.innerHTML = `
@@ -62,3 +42,4 @@ contacts.all.forEach( (contact) => {
 });
 
 
+// GET VALUES
